f0653a0092a7c56c245777af3e212c1b79d0066a
Branden Sherman <bshermandeveloper@gmail.com>
2020-11-05 13:44:13 -0500

    feat(server): created db and server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const { urlencoded } = require('express');
+const { db } = require('./db');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api', require('./api'));
+
+const init = async () => {
+  await db.sync();
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+};
+
+init();
